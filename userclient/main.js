ee9f2a9a0ef57b58582fa26555783c7e8871a972
MeinMex <78362012+Simeonya@users.noreply.github.com>
2024-03-12 17:01:29 +0100

    JSScript code changed to classes and optimization
    Added range for screen width

--- userclient/main.js
+++ userclient/main.js
@@ -1,85 +1,107 @@
-const userVideoElement = document.querySelector('#localVideo');
-const partnerVideoElement = document.querySelector('#remoteVideo');
-const textAreaElement = document.querySelector("textarea");
+class VideoSharing {
+  constructor() {
+    this.userVideoElement = document.querySelector('#localVideo');
+    this.partnerVideoElement = document.querySelector('#remoteVideo');
+    this.textAreaElement = document.querySelector("textarea");
+    this.video = document.getElementById('localVideo');
+    this.slider = document.getElementById('sizeSlider');
+    this.isSharing = false;
 
-const peerConnection = new RTCPeerConnection({
-  configuration: {
-    offerToReceiveAudio: false,
-    offerToReceiveVideo: true
-  },
-  iceServers: [
-
-    {
-      // example
-      urls: "turn:turn.example.org",
-      username: "user",
-      credential: "myPassword"
-    },
-
-  ]
-});
-
-peerConnection.onicecandidate = (event) => {
-  if (event.candidate) {
-    console.log(JSON.stringify(event.candidate));
-  }
-};
-
-peerConnection.ontrack = (event) => {
-  partnerVideoElement.srcObject = event.streams[0];
-};
-
-const displayMediaOptions = {
-  video: {
-    cursor: "always",
-    frameRate: { max: 15 },
-    height: { max: 720 }
-  },
-  audio: false
-};
-
-async function getDisplayMedia() {
-  const stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-  userVideoElement.srcObject = stream;
-  peerConnection.addStream(userVideoElement.srcObject);
-}
-
-getDisplayMedia().catch(() => {
-  console.log('errors with the media device');
-});
-
-function createOffer() {
-  peerConnection.createOffer({
-    mandatory: {
-      offerToReceiveAudio: false,
-      offerToReceiveVideo: true
-    },
-  })
-    .then(sessionDescription => {
-      console.log(JSON.stringify(sessionDescription));
-      peerConnection.setLocalDescription(sessionDescription);
+    this.peerConnection = new RTCPeerConnection({
+      configuration: {
+        offerToReceiveAudio: false,
+        offerToReceiveVideo: true
+      },
+      iceServers: [
+        {
+          // example
+          urls: "turn:turn.example.org",
+          username: "user",
+          credential: "myPassword"
+        },
+      ]
     });
-}
 
-function setRemoteDescription() {
-  const description = JSON.parse(textAreaElement.value);
-  peerConnection.setRemoteDescription(new RTCSessionDescription(description));
-}
+    this.peerConnection.onicecandidate = (event) => {
+      if (event.candidate) {
+        console.log(JSON.stringify(event.candidate));
+      }
+    };
 
-function createAnswer() {
-  peerConnection.createAnswer({
-    mandatory: {
-      offerToReceiveAudio: false,
-      offerToReceiveVideo: true
+    this.peerConnection.ontrack = (event) => {
+      this.partnerVideoElement.srcObject = event.streams[0];
+    };
+
+    this.displayMediaOptions = {
+      video: {
+        cursor: "always",
+        frameRate: { max: 15 },
+        height: { max: 720 }
+      },
+      audio: false
+    };
+
+    this.slider.oninput = () => {
+      this.video.style.width = this.slider.value + 'px';
     }
-  })
-    .then(sessionDescription => {
-      console.log(JSON.stringify(sessionDescription));
-      peerConnection.setLocalDescription(sessionDescription);
-    });
+  }
+
+  async getDisplayMedia() {
+    if (this.isSharing) {
+      return;
+    }
+    this.isSharing = true;
+    try {
+      const useAudio = false; // not implemented yet
+      const stream = await navigator.mediaDevices.getDisplayMedia({
+        ...this.displayMediaOptions,
+        audio: useAudio
+      });
+      this.userVideoElement.srcObject = stream;
+      this.peerConnection.addStream(this.userVideoElement.srcObject);
+    } catch (error) {
+      console.log('errors with the media device');
+      this.isSharing = false;
+    }
+  }
+
+  createOffer() {
+    this.peerConnection.createOffer({
+      mandatory: {
+        offerToReceiveAudio: false,
+        offerToReceiveVideo: true
+      },
+    })
+      .then(sessionDescription => {
+        console.log(JSON.stringify(sessionDescription));
+        this.peerConnection.setLocalDescription(sessionDescription);
+      });
+  }
+
+  setRemoteDescription() {
+    const description = JSON.parse(this.textAreaElement.value);
+    this.peerConnection.setRemoteDescription(new RTCSessionDescription(description));
+  }
+
+  createAnswer() {
+    this.peerConnection.createAnswer({
+      mandatory: {
+        offerToReceiveAudio: false,
+        offerToReceiveVideo: true
+      }
+    })
+      .then(sessionDescription => {
+        console.log(JSON.stringify(sessionDescription));
+        this.peerConnection.setLocalDescription(sessionDescription);
+      });
+  }
+
+  addCandidate() {
+    const candidate = JSON.parse(this.textAreaElement.value);
+    this.peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+  }
+
 }
 
-function addCandidate() {
-  const candidate = JSON.parse(textAreaElement.value);
-  peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-}
+const videoSharing = new VideoSharing();
+videoSharing.getDisplayMedia();
